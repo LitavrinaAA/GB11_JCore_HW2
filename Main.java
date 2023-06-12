0c645f3b2d6c99316b4d5a5eb8cc5b9a46943725
ShuMAhd <shuman@zykaika.ru>
2023-06-12 11:59:58 +0800

    два соседних элемента, с нулевым значением

--- Main.java
+++ Main.java
@@ -4,28 +4,42 @@ public class Main {
 
   public static void main(String[] args) {
     int[] nums1 = {2, 1, 2, 3, 4};
-    int[] nums2 = {2, 2, 0};
+    int[] nums2 = {2, 2, 0, 0};
     int[] nums3 = {1, 3, 5};
+
     System.out.println("Количество чётных элементов");
-    System.out.print(Arrays.toString(nums1) + "=" );
+    System.out.print(Arrays.toString(nums1) + "=");
     System.out.println(countEvens(nums1));
-    System.out.print(Arrays.toString(nums2) + "=" );
+    System.out.print(Arrays.toString(nums2) + "=");
     System.out.println(countEvens(nums2));
-    System.out.print(Arrays.toString(nums3) + "=" );
+    System.out.print(Arrays.toString(nums3) + "=");
     System.out.println(countEvens(nums3));
+
     System.out.println();
+
     System.out.println("Разница между мин. и макс.");
-    System.out.print(Arrays.toString(nums1) + "=" );
+    System.out.print(Arrays.toString(nums1) + "=");
     System.out.println(findDifference(nums1));
-    System.out.print(Arrays.toString(nums2) + "=" );
+    System.out.print(Arrays.toString(nums2) + "=");
     System.out.println(findDifference(nums2));
-    System.out.print(Arrays.toString(nums3) + "=" );
+    System.out.print(Arrays.toString(nums3) + "=");
     System.out.println(findDifference(nums3));
+
+    System.out.println();
+
+    System.out.println("Имеются ли два соседних элемента, с нулевым значением");
+    System.out.print(Arrays.toString(nums1) + "=");
+    System.out.println(hasZeroNeighbors(nums1));
+    System.out.print(Arrays.toString(nums2) + "=");
+    System.out.println(hasZeroNeighbors(nums2));
+    System.out.print(Arrays.toString(nums3) + "=");
+    System.out.println(hasZeroNeighbors(nums3));
   }
 
   /**
-   * Написать метод, возвращающий количество чётных элементов массива.
-   * countEvens([2, 1, 2, 3, 4]) → 3 countEvens([2, 2, 0]) → 3 countEvens([1, 3, 5]) → 0
+   * Написать метод, возвращающий количество чётных элементов массива. countEvens([2, 1, 2, 3, 4]) →
+   * 3 countEvens([2, 2, 0]) → 3 countEvens([1, 3, 5]) → 0
+   *
    * @param nums Принимает массив
    * @return возвращает количество чётных элементов массива.
    */
@@ -41,10 +55,11 @@ public class Main {
   }
 
   /**
-   * Написать функцию, возвращающую разницу между самым большим
-   * и самым ма- леньким элементами переданного не пустого массива.
+   * Написать функцию, возвращающую разницу между самым большим и самым ма- леньким элементами
+   * переданного не пустого массива.
+   *
    * @param nums Принимает массив
-   * @return
+   * @return Возвращаем разницу между максимальным и минимальным элементами
    */
   public static int findDifference(int[] nums) {
     int max = nums[0];  // Предполагаем, что первый элемент - максимальный
@@ -62,5 +77,22 @@ public class Main {
 
     return max - min;  // Возвращаем разницу между максимальным и минимальным элементами
   }
+
+  /**
+   * Написать функцию, возвращающую истину, если в переданном массиве есть два соседних элемента, с
+   * нулевым значением.
+   *
+   * @param nums Принимает массив
+   * @return Возвращает true или false
+   */
+  public static boolean hasZeroNeighbors(int[] nums) {
+    for (int i = 0; i < nums.length - 1; i++) {
+      if (nums[i] == 0 && nums[i + 1] == 0) {
+        return true;  // Если найдены два соседних элемента со значением 0, возвращаем true
+      }
+    }
+    return false;  // Если такие элементы не найдены, возвращаем false
+  }
+
 
 }
