ebad724a5790a9c55a9a1ac7e476604642b35ead
LitavrinaA <a.a.litavrina@gmail.com>
2024-02-04 21:53:06 +0300

    v1

--- ru/shum/CountEvens.java
+++ ru/shum/CountEvens.java
@@ -2,13 +2,13 @@ package ru.shum;
 
 
 public class CountEvens {
+  private static int count = 0;
   /**
    * Написать метод, возвращающий количество чётных элементов массива. countEvens([2, 1, 2, 3, 4]) →
    * 3 countEvens([2, 2, 0]) → 3 countEvens([1, 3, 5]) → 0
    * @param arrayOfNums Принимает массив
    * @return возвращает количество чётных элементов массива.
    */
-  private static int count = 0;
   public static int countEvens(int[] arrayOfNums) {
     for (int num : arrayOfNums) {
       if (num % 2 == 0) {
